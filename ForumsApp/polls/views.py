bea9c9049c375810cac228b8265779cadf9cc38e
polinaidu007 <15pa1a05f8@vishnu.edu.in>
2018-07-17 20:58:57 +0530

    render html pages

--- ForumsApp/polls/views.py
+++ ForumsApp/polls/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from .models import Question
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:2]
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         "latest_question_list" : latest_question_list,
     }
     return render(request,'polls/index.html',context)
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
 # Create your views here.
